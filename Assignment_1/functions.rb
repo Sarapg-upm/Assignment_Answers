166908e9a16f5e14c8439e756d9c3f337f049a6a
Sarapg-upm <saraperezgarc@gmail.com>
2019-10-22 09:50:34 -0400

    commiting

--- Assignment_1/functions.rb
+++ Assignment_1/functions.rb
@@ -1,7 +1,10 @@
 require 'csv'
 require 'date'
+require 'fileutils'
+include FileUtils
 
 def read_csv (path, header = true, sep = "\t")
+  
   
   my_csv = CSV.read(path, headers: header,  :col_sep => sep)
   
@@ -9,6 +12,10 @@ def read_csv (path, header = true, sep = "\t")
 end
 
 def write_csv(path, row, sep = "\t", header)
+  
+  if CSV.read(path)
+    rm %w(path)
+  end
   csv = CSV.open(path, "a+", :write_headers=> true, :headers => header, :col_sep => sep)
   csv << row
 end
