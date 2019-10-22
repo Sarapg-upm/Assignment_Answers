7511a905e9dfc1d5845bb8ae1c09f80841084a29
Sarapg-upm <saraperezgarc@gmail.com>
2019-10-22 10:47:36 -0400

    commiting

--- Assignment_1/functions.rb
+++ Assignment_1/functions.rb
@@ -1,23 +1,19 @@
 require 'csv'
 require 'date'
-require 'fileutils'
-include FileUtils
+
 
 def read_csv (path, header = true, sep = "\t")
-  
-  
+
   my_csv = CSV.read(path, headers: header,  :col_sep => sep)
   
   return my_csv
 end
 
-def write_csv(path, row, sep = "\t", header)
-  
-  if CSV.read(path)
-    rm %w(path)
+def write_csv(path, data, sep = "\t", header)
+  csv = CSV.open(path, "w", :write_headers=> true, :headers => header, :col_sep => sep)
+  for row in data 
+    csv << row
   end
-  csv = CSV.open(path, "a+", :write_headers=> true, :headers => header, :col_sep => sep)
-  csv << row
 end
 
 def is_datetime(d)
