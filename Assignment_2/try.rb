3bb36d9917fd7d9411669cd153af2bee786000ba
Sarapg-upm <saraperezgarc@gmail.com>
2019-11-06 13:31:23 -0500

    almost final code

--- Assignment_2/try.rb
+++ Assignment_2/try.rb
@@ -2,62 +2,81 @@ require 'rest-client'   # this is how you access the Web
 require './functions.rb'
 require 'ruby-progressbar'
 require 'json'
+require './AT_Gene.rb'
+$VERBOSE=nil
 
-
-#my_codes = read_csv('./ArabidopsisSubNetwork_GeneList.txt', false, "\n").flatten!
-
-my_codes = read_csv('./target-codes.tsv', false, "\n").flatten!
-
-TAIR_code = Regexp.new(/AT\dG\d{0,5}/i)
-interactors = Array.new
-
-progressbar = ProgressBar.create(:total => my_codes.size)
-
-uppercase(my_codes)
-puts "Getting data...\n"
-
-for code in my_codes
-
-  tab25 = "http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{code}?format=tab25"
-  
-  res = fetch(tab25)
-
-  body = res.body  # get the "body" of the response
-  all_codes = body.scan(TAIR_code)
-
-  uppercase(all_codes)
-  check_codes = all_codes & my_codes
-  check_codes -= [code]
-  if check_codes.any?
-    interactors += check_codes
-    interactors += [code]
-  end
-  
-  progressbar.increment
-  sleep 0.1
-
+data = AT_Gene.insert_data('./target-codes.tsv')
+for object in data
+  puts object.TAIR_ID
+  print object.get_data
+  puts
 end
-
-interactors.uniq!
-
-for code in interactors
-  kegg_gene = "http://togows.org/entry/kegg-genes/ath:#{code}.json"
-  GO_query = "http://togows.dbcls.jp/entry/uniprot/#{code}/dr.json" 
-  
-  res = fetch(kegg_gene)
-  data = JSON.parse(res.body)
-  
-  res2 = fetch(GO_query)
-  data_GO = JSON.parse(res2.body)
-  
-  puts "This code #{code}"
-  for elem in data[0]["pathways"].each
-  #   puts elem
-    puts "\tKEGG_Pathway ID: #{elem[0]}  name: #{elem[1]}"
-  end
-  for go in data_GO[0]["GO"]
-    if (go[1]=~ /^P:/)
-      puts "GO_ID:#{go[0]}\tname: #{go[1]}"
-    end
-  end
-end
+#
+##my_codes = read_csv('./ArabidopsisSubNetwork_GeneList.txt', false, "\n").flatten!
+#
+#my_codes = read_csv('./target-codes.tsv', false, "\n").flatten!
+#
+#TAIR_code = Regexp.new(/AT\dG\d{0,5}/i)
+#interactors = Array.new
+#
+#progressbar = ProgressBar.create(:total => my_codes.size,
+#                                 :format => '%t %a |%b|%i| %p%% ',
+#                                 :progress_mark  => '=',
+#                                 :remainder_mark => " ",
+#                                 :title => "Getting Data ")
+#
+#uppercase(my_codes)
+##puts "Getting data...\n"
+#
+#for code in my_codes
+#
+#  tab25 = "http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/interactor/#{code}?format=tab25"
+#  
+#  res = fetch(tab25)
+#
+#  #body = res.body  # get the "body" of the response
+#  all_codes = res.body.scan(TAIR_code)
+#
+#  uppercase(all_codes)
+#  check_codes = all_codes & my_codes
+#  check_codes -= [code]
+#  
+#  #check_codes.any? ? (interactors += check_codes) :
+#  #interactors += check_codes unless check_codes.empty?
+#  if check_codes.any?
+#    interactors += check_codes
+#    interactors += [code]
+#  end
+#  #
+#  progressbar.increment
+#  sleep 0.1
+#
+#end
+#
+#interactors.uniq!
+#
+#for code in interactors
+#  kegg_gene = "http://togows.org/entry/kegg-genes/ath:#{code}.json"
+#  GO_query = "http://togows.dbcls.jp/entry/uniprot/#{code}/dr.json" 
+#  
+#  res = fetch(kegg_gene)
+#  data = JSON.parse(res.body)
+#  
+#  res2 = fetch(GO_query)
+#  data_GO = JSON.parse(res2.body)
+#  kegg_path, kegg_name = Array.new, Array.new
+#  puts "\nThis code #{code}"
+#  for elem in data[0]["pathways"]
+#     #print elem
+#     kegg_path <<  elem[0]
+#     kegg_name << elem[1]
+#    #puts "\tKEGG_Pathway ID: #{elem[0]}  name: #{elem[1]}"
+#  end
+#  print kegg_path
+#  print kegg_name
+#  #for go in data_GO[0]["GO"]
+#  #  if (go[1]=~ /^P:/)
+#  #    puts "GO_ID:#{go[0]}\tname: #{go[1]}"
+#  #  end
+#  #end
+#end
