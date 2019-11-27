1a8d2c1e869de9351bbb6df88f7ab8a3f0157631
Sarapg-upm <saraperezgarc@gmail.com>
2019-11-27 11:44:11 -0500

    this doesnt work properly

--- Assignment_3/crazy.rb
+++
@@ -1,2 +0,0 @@
-require'ensembl-api'
-#include Ensembl::Core
--- Assignment_3/my_code.rb
+++ Assignment_3/my_code.rb
@@ -1,17 +1,21 @@
-require './Interaction_Network.rb'
+
 require './functions.rb'
 require 'net/http'   # this is how you access the Web
 require 'bio'
-require 'enumerator'
 
-#https://www.ebi.ac.uk/Tools/dbfetch/dbfetch?db=ensemblgenomesgene&id=AT5g54270&format=embl
+puts "Reading file"
 
 my_codes = read_csv('./try_codes.txt', false, "\t").flatten!
-#my_codes_string = my_codes.join(",")
-#print my_codes_string
 target_sequence_forward = Bio::Sequence::NA.new("CTTCTT")
 target_sequence_reverse = target_sequence_forward.complement
 
+data_matched_exons = []
+data_matched_ch = []
+unmatched_codes_exons = []
+unmatched_codes_gene = []
+
+puts "Accessing EnsEMBL"
+puts "Searching for target sequence 'CTTCTT' in exons genes..."
 for code in my_codes
 
   entry = fetch_embl(code)
@@ -20,55 +24,60 @@ for code in my_codes
   
   entry.features.each do |feature|
     next unless (feature.feature == "exon" && (not feature.position =~ /[A-Z]/))
-    
-    #next unless exon.scan(target_sequence).each do |target_found|
-    #  start_position, end_position = $~.offset(0) #$~ : The MatchData of the last Regexp match in the current scope.
-    #  exon_locations = feature.locations()
-    #  exon_locations.each do |loc|
-    #    puts "range = #{loc.from}..#{loc.to} (strand = #{loc.strand})"
-    #  end
-    #end
-    
-    exons = feature.locations()
-    
+    exons = feature.locations()  
     exons.each do |exon|
+      exon_sequence = gene_sequence.splicing("#{exon.from}..#{exon.to}")
       if exon.strand == -1
-        exon_sequence = gene_sequence.splicing("#{exon.from}..#{exon.to}")
-        next unless exon_sequence.scan(/#{target_sequence_reverse.to_s}/i).each do |target_found|
-          puts target_found
-
-          for matches in Regexp.last_match.offset(0)
-            puts matches
+        if exon_sequence.scan(/#{target_sequence_reverse.to_s}/i).each do |target_found|
+            start_position = $~.offset(0).first
+            end_position = $~.offset(0).last
+            match_position_from = exon.from + start_position + 1
+            match_position_to = exon.from + end_position + 1
+            data_matched_exons << [code, "source", "target-CTTCTT", match_position_from, match_position_to,".", "-", ".", "gene_id:#{code}; transcript:complement(#{exon.from}..#{exon.to}); target: CTTCTT"]
           end
-          #start_position, end_position = $~.offset(0) #$~ : The MatchData of the last Regexp match in the current scope.
-        #  puts "exn sequence\n #{exon_sequence}"  
-        #  puts "STAR POSITION #{start_position}"
-        #  puts "END POSITION #{end_position}"
+        else
+          unmatched_codes_exons << code
+        end
+      else #exon is +
+        if exon_sequence.scan(/#{target_sequence_forward.to_s}/i).each do |target_found|
+            start_position = $~.offset(0).first
+            end_position = $~.offset(0).last
+            match_position_from = exon.from + start_position + 1
+            match_position_to = exon.from + end_position + 1
+            data_matched_exons << [code, "source", "target-CTTCTT", match_position_from, match_position_to,".", "+", ".", "gene_id:#{code}; transcript:#{exon.from}..#{exon.to}; target: CTTCTT"]
+          end
+        else
+          unmatched_codes_exons << code
         end
       end
     end
-    
-    #puts gene_sequence.output(:fasta)
-    #
-    #exon = gene_sequence.splicing(feature.position) #  If position of feature doesn't fit with gene sequence length, next feature
-    #
-    #puts exon
-    #
-    #exon_in_gene = feature.locations() # Bio::Locations object (container for Bio::Location objects)
-    #
-    #exon.scan(target_sequence) do |repetition| # Search motif in the exon sequence
-    #  from=$~.offset(0).first
-    #  to=$~.offset(0).last
-    #  
-    #  if exon_in_gene.first.strand == -1 # Depending on strand sign, get coordinates
-    #    length = to - from # Length of motif
-    #    to = exon_in_gene.first.to - from + 1 # Where repetition ends in gene
-    #    from = to - length + 1 # Where repetition starts in gene
-    #    new_coord_chr = "#{from + chr_start - 2}..#{to + chr_start - 2 }" # Get coordinates in chromosome
-    #    new_coord_gene="#{from - 1}..#{to - 1}" # Get coordinates in gene
-    #    puts gene_sequence.splicing(new_coord_gene)
-    #  end
-    #end
-    
   end
-end
+  
+  if gene_sequence.scan(/#{target_sequence_forward.to_s}/i).each do |target_found|    
+    start_position = $~.offset(0).first
+    end_position = $~.offset(0).last
+    match_position_from = chr_start + start_position + 1
+    match_position_to = chr_end + end_position + 1
+    data_matched_ch << [chr_n, "source", "target-CTTCTT", match_position_from, match_position_to,".", ".", ".", "gene_id:#{code}; target: CTTCTT"] 
+  
+    end
+  else
+    unmatched_codes_gene << code
+  end
+end   
+
+write_csv("./target_found_exons.gff", data_matched_exons, "\t", "##gff-version 3")
+
+puts "GFF file with target sequences found in the exons anc be found at './target_found_exons.gff'"
+
+write_csv("./target_found_ch.gff", data_matched_ch, "\t", "##gff-version 3")
+
+puts "GFF file with target sequences found in the exons anc be found at './target_found_ch.gff'"
+
+write_csv("./unmatched_codes_exons.csv", unmatched_codes_exons, "\t", "unmatched_codes_exons")
+
+puts "Unmatched genes './unmatched_codes_exons.csv'"
+
+write_csv("./unmatched_codes_gene.csv", unmatched_codes_gene, "\t", "unmatched_codes_gene")
+
+puts "Unmatched genes './unmatched_codes_gene.csv'"
